142a4af8a225082657b79d1e341f96eb88be4a71
Franchise2007 <63330595+Franchise2007@users.noreply.github.com>
2020-06-02 13:37:56 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA4S6FPHQ4HC2MYNND"
-  secret_key = "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"
+  access_key = "AKIA4S6FPHQ4EUIEQXMH"
+  secret_key = "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"
 }
